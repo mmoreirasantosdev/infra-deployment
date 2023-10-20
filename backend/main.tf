410b259cdbb04ce9ce164d7b6133a2427e90a4b9
Marcos Moreira <marcos@facings.io>
2023-10-20 18:37:08 -0300

    Adicionando estrutura inicial do projeto

--- backend/main.tf
+++ backend/main.tf
@@ -1,6 +1,6 @@
 /*terraform {
   #############################################################
-  ## AFTER RUNNING TERRAFORM APPLY (WITH LOCAL BACKEND)
+  ## APOS EXECUTAR O TERRAFORM  (WITH LOCAL BACKEND)
   ## YOU WILL UNCOMMENT THIS CODE THEN RERUN TERRAFORM INIT
   ## TO SWITCH FROM LOCAL BACKEND TO REMOTE AWS BACKEND
   #############################################################
@@ -20,12 +20,15 @@
   }
 }
 */
+
+
 provider "aws" {
-  region = "us-east-1"
+  region = "us-west-2"
 }
 
+
 resource "aws_s3_bucket" "terraform_state" {
-  bucket        = "challenge-devops-directive-tf-state" # REPLACE WITH YOUR BUCKET NAME
+  bucket        = "challenge-devops-tf-state"
   force_destroy = true
 }
 
@@ -37,7 +40,7 @@ resource "aws_s3_bucket_versioning" "terraform_bucket_versioning" {
 }
 
 resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state_crypto_conf" {
-  bucket        = aws_s3_bucket.terraform_state.bucket
+  bucket = aws_s3_bucket.terraform_state.bucket
   rule {
     apply_server_side_encryption_by_default {
       sse_algorithm = "AES256"
