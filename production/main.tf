67a765af44ba7e78957185e9c032bc3b053bcf4f
Marcos Moreira <marcos@facings.io>
2023-10-20 20:29:43 -0300

    Adicionando state para os ambientes

---
+++ production/main.tf
@@ -0,0 +1,20 @@
+terraform {
+  backend "s3" {
+    bucket         = "challenge-devops-tf-state"
+    key            = "challenge/devops/production/terraform.tfstate"
+    region         = "us-west-2"
+    dynamodb_table = "terraform-state-locking"
+    encrypt        = true
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-west-2"
+}
